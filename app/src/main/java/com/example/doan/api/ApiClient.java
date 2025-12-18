f815dda684a73eb9443c02bd953795088773f4a4
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2025-12-18 14:29:21 +0700

    chore(network): customize OkHttpClient timeout

--- app/src/main/java/com/example/doan/api/ApiClient.java
+++ app/src/main/java/com/example/doan/api/ApiClient.java
@@ -1,13 +1,11 @@
 package com.example.doan.api;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-/**
- * ApiClient
- * - Khởi tạo Retrofit
- * - Kết nối tới Backend tự dựng
- */
 public class ApiClient {
 
     private static final String BASE_URL = "http://10.0.2.2:5000/api/";
@@ -15,8 +13,16 @@ public class ApiClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
+
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS) // kết nối
+                    .readTimeout(60, TimeUnit.SECONDS)    // đọc response (QUAN TRỌNG)
+                    .writeTimeout(30, TimeUnit.SECONDS)   // gửi request
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }

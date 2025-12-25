de881ced233dabec51cb2bf381884303e3c498ab
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2025-12-25 20:01:10 +0700

    fix(something): too tired

--- app/src/main/java/com/example/doan/OpportunityDetailActivity.java
+++ app/src/main/java/com/example/doan/OpportunityDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -46,6 +47,8 @@ public class OpportunityDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_opportunity_detail);
 
         mapping();
+
+        tvContent.setMovementMethod(LinkMovementMethod.getInstance());
 
         // Init Markwon
         markwon = Markwon.create(this);
